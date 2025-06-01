4cf373a4547ab7fd424f2a65fd3cb8fe8ed34153
Jonathan Sharpe <jonathan@codeyourfuture.io>
2025-06-01 17:51:44 +0100

    Improve setup
    - Install and configure live server
    - Fix installation requirements
    - Rationalise git ignore files
    - Source .env when loading DB

--- db/create-schema.sh
+++ db/create-schema.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-export PGPASSWORD="$(cat "${SCRIPT_DIR}/../backend/.env" | grep ^PGPASSWORD= | cut -d= -f2-)"
+source "$SCRIPT_DIR/../backend/.env"
 
-psql -h 127.0.0.1 -U postgres -f "${SCRIPT_DIR}/schema.sql"
+psql \
+  --dbname "${PGDATABASE:-postgres}" \
+  --file "${SCRIPT_DIR}/schema.sql" \
+  --host "${PGHOST:-127.0.0.1}" \
+  --port "${PGPORT:-5432}" \
+  --username "${PGUSER:-postgres}"
